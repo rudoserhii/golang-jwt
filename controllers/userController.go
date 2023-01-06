f93814ef62302f159c84f1e5eec93036edfd6510
rudoserhii <rudoserhii@gmail.com>
2023-01-06 11:36:04 +0000

    worked on helpers package and controllers package

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,24 +1,141 @@
 package controllers
 
 import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
 	"github.com/fredele20/golang-jwt-project/database"
+	"github.com/fredele20/golang-jwt-project/helpers"
+	"github.com/fredele20/golang-jwt-project/models"
+	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/crypto/bcrypt"
 )
-
-
 
 var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
 var validate = validator.New()
 
- func HashPassword() {}
+func HashPassword() {}
 
- func VerifyPassword() {}
+func VerifyPassword(userPassword, providedPassword string) (bool, string) {
+	err := bcrypt.CompareHashAndPassword([]byte(providedPassword), []byte(userPassword))
+	check := true
+	msg := ""
+	if err != nil {
+		msg = fmt.Sprintf("email or password is incorrect")
+		check = false
+	}
 
- func Signup() {}
+	return check, msg
+}
 
- func Login() {}
+func Signup() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		var userCtx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		var user models.User
 
- func GetUsers() {}
+		if err := ctx.BindJSON(&user); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
 
- func GetUser() {}
+		validationErr := validate.Struct(user)
+		if validationErr != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
+			return
+		}
+
+		count, err := userCollection.CountDocuments(userCtx, bson.M{"email": user.Email})
+		defer cancel()
+		if err != nil {
+			log.Panic(err)
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while checking for the email"})
+		}
+
+		count, err = userCollection.CountDocuments(userCtx, bson.M{"phone": user.Phone})
+		defer cancel()
+		if err != nil {
+			log.Panic(err)
+			ctx.JSON(http.StatusInternalServerError, gin.H{"erro": "error occured while checking for the phone number"})
+		}
+
+		if count > 0 {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": "user with this phone number or email already exists"})
+		}
+
+		user.Created_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		user.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		user.ID = primitive.NewObjectID()
+		user.User_id = user.ID.Hex()
+
+		token, refereshToken, err := helpers.GenerateAuthToken(*user.Email, *user.First_name, *user.Last_name, *user.User_type, *&user.User_id)
+		if err != nil {
+			log.Panic(err)
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while generating token"})
+		}
+		user.Token = &token
+		user.Refresh_token = &refereshToken
+
+		resultInsertionNumber, insertError := userCollection.InsertOne(userCtx, user)
+		if insertError != nil {
+			msg := fmt.Sprintf("User item was not created")
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			return
+		}
+		defer cancel()
+		ctx.JSON(http.StatusOK, resultInsertionNumber)
+	}
+}
+
+func Login() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		var userCtx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		var user models.User
+		var foundUser models.User
+
+		if err := ctx.BindJSON(&user); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		err := userCollection.FindOne(userCtx, bson.M{"email": user.Email}).Decode(&foundUser)
+		defer cancel()
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": "email or password is incorrect"})
+			return
+		}
+
+		validPassword, msg := VerifyPassword(*user.Password, *foundUser.Password)
+		defer cancel()
+	}
+}
+
+func GetUsers() {}
+
+// only the admin has the acces to this request
+func GetUser() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		userId := ctx.Param("user_id")
+
+		if err := helpers.MatchUserTypeToUid(ctx, userId); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		var userCtx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+		var user models.User
+		err := userCollection.FindOne(userCtx, bson.M{"user_id": userId}).Decode(&user)
+		defer cancel()
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		ctx.JSON(http.StatusOK, user)
+	}
+}
---
+++ helpers/tokenHelper.go
@@ -0,0 +1,57 @@
+package helpers
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/fredele20/golang-jwt-project/database"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type SignedDetails struct {
+	Email      string
+	First_name string
+	Last_name  string
+	Uid        string
+	User_type  string
+	jwt.StandardClaims
+}
+
+var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
+
+var SECRET_KEY string = os.Getenv("SECRET_KEY")
+
+func GenerateAuthToken(email, firstname, lastname, userType, uid string) (signedToken, signedRefreshToken string, err error) {
+	claims := &SignedDetails{
+		Email: email,
+		First_name: firstname,
+		Last_name: lastname,
+		Uid: uid,
+		User_type: userType,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(24)).Unix(),
+		},
+	}
+
+	refreshClaims := &SignedDetails{
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(168)).Unix(),
+		},
+	}
+
+	token, err := jwt.NewWithClaims(jwt.SigningMethodES256, claims).SignedString([]byte(SECRET_KEY))
+	if err != nil {
+		log.Panic(err)
+		return
+	}
+	
+	refreshToken, err := jwt.NewWithClaims(jwt.SigningMethodES256, refreshClaims).SignedString([]byte(SECRET_KEY))
+	if err != nil {
+		log.Panic(err)
+		return
+	}
+
+	return token, refreshToken, err
+}
